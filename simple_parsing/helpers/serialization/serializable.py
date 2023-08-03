89dd27a36f197855f6842b5040b8bb05dc050f59
Fabrice Normandin <normandf@mila.quebec>
2023-08-03 18:06:03 -0400

    Fix a bug with parsing with _type_ keys in config
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- simple_parsing/helpers/serialization/serializable.py
+++ simple_parsing/helpers/serialization/serializable.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import functools
 
 import json
 import pickle
@@ -816,6 +817,7 @@ def from_dict(
         if name not in obj_dict:
             if (
                 field.metadata.get("to_dict", True)
+                and field.init
                 and field.default is MISSING
                 and field.default_factory is MISSING
             ):
@@ -912,6 +914,7 @@ def is_dataclass_or_optional_dataclass_type(t: type) -> bool:
     return is_dataclass(t) or (is_optional(t) and is_dataclass(get_args(t)[0]))
 
 
+@functools.lru_cache(maxsize=None)
 def _locate(path: str) -> Any:
     """
     COPIED FROM Hydra:
